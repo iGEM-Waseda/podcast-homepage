9f8d92206c04d976d53014ad160043d30713108e
kiha40-777 <y.toriyama183501@gmail.com>
2026-06-01 00:07:00 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
       <p className="text-5xl text-center pt-8">あいじぇむわせだの、バイオな毎日</p>
       <p className="text-5xl text-center pt-8">Podcast What is iGEM - Waseda?</p>
+      <p className="text-5xl text-center pt-8">あいじぇむわせだの、バイオな毎日</p>
     </>
   );
 }
