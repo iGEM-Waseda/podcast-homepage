3a1b34d498d2b7b1196291ced1288547e5a18c66
kiha40-777 <y.toriyama183501@gmail.com>
2026-06-07 20:15:35 +0900

    色名の変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <section className="relative w-full h-screen pt-[84px] xl:pt-[108px] flex justify-center overflow-hidden bg-herobackground">
+      <section className="relative w-full h-screen pt-[84px] xl:pt-[108px] flex justify-center overflow-hidden bg-customblack">
         <p className="text-6xl text-center pt-8 text-white font-medium">あいじぇむわせだの、バイオな毎日</p>
       </section>
       <p className="text-5xl text-center pt-8">Podcast What is iGEM - Waseda?</p>
