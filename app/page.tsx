4e2d7ac10c7ee5805e6f1fa0b9ad3f323ebb4e67
kiha40-777 <y.toriyama183501@gmail.com>
2026-06-07 14:28:02 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <p className="text-5xl text-center pt-8">あいじぇむわせだの、バイオな毎日</p>
+      <section className="relative w-full h-screen pt-[84px] xl:pt-[108px] flex justify-center overflow-hidden bg-herobackground">
+        <p className="text-6xl text-center pt-8 text-white font-medium">あいじぇむわせだの、バイオな毎日</p>
+      </section>
       <p className="text-5xl text-center pt-8">Podcast What is iGEM - Waseda?</p>
       <p className="text-5xl text-center pt-8">あいじぇむわせだの、バイオな毎日</p>
     </>
