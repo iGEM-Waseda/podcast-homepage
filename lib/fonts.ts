266aa7bccfafc4324cb0c591f0e1d9a5965a5328
kiha40-777 <y.toriyama183501@gmail.com>
2026-06-20 23:40:26 +0900

    add comment

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,17 +1,20 @@
 import { Caveat, Londrina_Outline, Kiwi_Maru } from 'next/font/google';
 
+// ヘッダーの手書き風文字
 export const caveat = Caveat({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
   variable: '--font-caveat'
 });
 
+// DNA配列のマルキーで使用
 export const londrinaOutline = Londrina_Outline({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-londrina-outline'
 });
 
+// 標準フォント
 export const kiwiMaru = Kiwi_Maru({
     subsets: ['latin'],
     weight: ['400', '500'],
